40a98fbcc6db595c8c6b18321169a8109a9097db
문승준 <munseungjun@munseungjun-ui-MacBookAir.local>
2023-03-22 16:52:44 +0900

    #START - git start

---
+++ front/reducers/index.js
@@ -0,0 +1,20 @@
+import { ConsoleSqlOutlined } from '@ant-design/icons';
+import { HYDRATE } from 'next-redux-wrapper';
+
+import user from './user';
+import post from './post';
+import { combineReducers } from 'redux';
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      return action.payload;
+    default:
+      const combineReducer = combineReducers({
+        user,
+        post,
+      });
+      return combineReducer(state, action);
+  }
+};
+export default rootReducer;
